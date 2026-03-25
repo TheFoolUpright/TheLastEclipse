91f74738a1f3627be9f1bce8735cee4cb90a0080
Cintanaa <cintana@outlook.com>
2026-03-25 12:26:24 +0100

    Flipping Platform

--- Assets/Scripts/FlippingPlatform.cs
+++ Assets/Scripts/FlippingPlatform.cs
@@ -1,13 +1,53 @@
 using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 
 public class FlippingPlatform : MonoBehaviour
 {
-    [SerializeField] private Vector3 _rotation; 
-   
-    void Update()
+    [SerializeField] private float _rotationAmount = 90f;
+    [SerializeField] private float _speed = 90f;    
+    [SerializeField] private float _pauseTime = 1.5f;
+
+    public PlayerController Player;
+
+    void Start()
     {
-        //transform.Rotate(_rotation * );
+
+        if (Player.IsMoonActive)
+        {
+            {
+                StartCoroutine(RotateRoutine());
+            }
+
+            IEnumerator RotateRoutine()
+            {
+                while (true)
+                {
+                    Quaternion startRot = transform.rotation;
+                    Quaternion targetRot = startRot * Quaternion.Euler(_rotationAmount, 0, 0);
+
+                    while (transform.rotation != targetRot)
+                    {
+                        transform.rotation = Quaternion.RotateTowards(
+                            transform.rotation,
+                            targetRot,
+                            _speed * Time.deltaTime
+                        );
+
+                        yield return null;
+                    }
+
+                    yield return new WaitForSeconds(_pauseTime);
+                }
+            }
+        }
+
+        else
+        {
+
+        }
+
     }
 }
+
