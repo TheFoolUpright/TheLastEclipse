b68861a1fba299f7495ff9f926fc45f888d96660
Cintanaa <cintana@outlook.com>
2026-03-27 14:40:39 +0100

    changes flipping

--- Assets/Scripts/Platforms/FlippingPlatform.cs
+++ Assets/Scripts/Platforms/FlippingPlatform.cs
@@ -5,6 +5,7 @@ using Unity.VisualScripting;
 
 public class FlippingPlatform : MonoBehaviour
 {
+<<<<<<< Updated upstream
     [SerializeField] private float _rotationAmount = 90f;
     [SerializeField] private float _speed = 90f;    
     [SerializeField] private float _pauseTime = 1.5f;
@@ -51,3 +52,52 @@ public class FlippingPlatform : MonoBehaviour
     }
 }
 
+=======
+    [SerializeField] private float _speed;
+    [SerializeField] private float _pauseTime;
+
+    public PlayerController Player;
+   
+    IEnumerator RotateRoutine(bool IsMoonActive)
+    {
+        while (true)
+        {
+            Quaternion startRot = transform.rotation;
+            Quaternion targetRot;
+
+            if (IsMoonActive)
+            {
+                targetRot = startRot * Quaternion.Euler(90, 0, 0);
+            }
+
+            else
+            {
+                targetRot = startRot * Quaternion.Euler(-90, 0, 0);
+            }
+
+            while (transform.rotation != targetRot)
+            {
+                transform.rotation = Quaternion.RotateTowards(
+                    transform.rotation,
+                    targetRot,
+                    _speed * Time.deltaTime
+                );
+
+                yield return null;
+            }
+
+            yield return new WaitForSeconds(_pauseTime);
+        }
+
+    }
+
+    void Start()
+    {
+        StartCoroutine(RotateRoutine(Player.IsMoonActive));
+    }
+}
+                
+
+
+   
+>>>>>>> Stashed changes
