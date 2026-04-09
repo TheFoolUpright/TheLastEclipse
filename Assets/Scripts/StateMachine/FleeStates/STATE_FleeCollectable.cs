9c07bcf67d9edc0867f2744ba347e388b34dd7d0
Cintanaa <cintana@outlook.com>
2026-04-09 14:15:00 +0200

    Collectable state

--- Assets/Scripts/StateMachine/FleeStates/STATE_FleeCollectable.cs
+++ Assets/Scripts/StateMachine/FleeStates/STATE_FleeCollectable.cs
@@ -4,24 +4,51 @@ using UnityEngine;
 public class STATE_FleeCollectable : BaseState
 {
     private readonly FleeSoulAgent _owner;
-    
-    public STATE_FleeCollectable(FleeSoulAgent owner) : base(owner.gameObject) {
+
+    public STATE_FleeCollectable(FleeSoulAgent owner) : base(owner.gameObject)
+    {
         _owner = owner;
     }
 
     // Runs every frame
-    public override Type Tick() {
+    public override Type Tick()
+    {
+
+        // Check of er iets de trigger raakt
+        Collider[] hits = Physics.OverlapSphere(_owner.transform.position, 0.5f);
+
+        foreach (var hit in hits)
+        {
+            if (hit.CompareTag("Player"))
+            {
+                Debug.Log("Soul Collected!");
+
+                _owner.gameObject.SetActive(false);
+
+                return null;
+            }
+        }
+
         return null;
     }
-    
+
     // Runs when we enter this state
-    public override void OnEnter(BaseState oldState){
+    public override void OnEnter(BaseState oldState)
+    {
         _owner.ClearMovement();
         Debug.Log("Entered Collectable State");
+
+        // Zorg dat hij stil staat
+        Rigidbody rb = _owner.GetComponent<Rigidbody>();
+        if (rb != null)
+        {
+            rb.linearVelocity = Vector3.zero;
+        }
     }
-    
+
     // Runs when we exit this state
-    public override void OnExit(BaseState newState) {
-    
+    public override void OnExit(BaseState newState)
+    {
+
     }
 }
