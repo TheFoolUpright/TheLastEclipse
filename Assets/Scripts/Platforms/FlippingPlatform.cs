f4544839e79ecbbe34ddfca77d49ac1f87bc60eb
Cintanaa <cintana@outlook.com>
2026-04-11 14:30:59 +0200

    Collectable State finished

--- Assets/Scripts/Platforms/FlippingPlatform.cs
+++ Assets/Scripts/Platforms/FlippingPlatform.cs
@@ -11,6 +11,9 @@ public class FlippingPlatform : MonoBehaviour
     [SerializeField] private float _speed = 90f;
     [SerializeField] private float _pauseTime = 1.5f;
 
+
+
+
     public PlayerController Player;
 
     private void Awake()
@@ -34,7 +37,7 @@ public class FlippingPlatform : MonoBehaviour
             _rotationAmount = -90f;
     }
 
- 
+
     IEnumerator RotateRoutine()
     {
         while (true)
@@ -56,6 +59,41 @@ public class FlippingPlatform : MonoBehaviour
             yield return new WaitForSeconds(_pauseTime);
         }
     }
+
+
+
+    public float shakeDuration = 2;
+
+    private bool animating;
+    private Vector3 startPosition;
+    private Vector3 fallOffset = new Vector3(0, -100, 0);
+    private int animationStatus;
+    private float animationTimer;
+    private float animationDuration;
+
+    void FixedUpdate()
+    {
+        if (!animating) return;
+
+        animationTimer += Time.fixedDeltaTime;
+
+        if (animationStatus == 1)
+        {
+            transform.position = startPosition + new Vector3(Mathf.Sin(animationTimer * UnityEngine.Random.Range(1, 3)), 0, -Mathf.Sin(animationTimer * UnityEngine.Random.Range(1, 3)));
+
+        }
+        else if (animationStatus == 2)
+        {
+            //how much percent are we in the animation
+            float percentage = animationTimer / animationDuration;
+            transform.position = startPosition + fallOffset * percentage;
+
+        }
+
+
+
+    }
+
 
     //void Start()
     //{
